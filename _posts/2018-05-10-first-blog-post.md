a403dfff23b502c66211df279db5ee793d7fd3d8
Max Wirt <max@sincerely.tv>
2018-05-15 16:31:02 -0400

    set up blog

--- _posts/2018-05-10-first-blog-post.md
+++ _posts/2018-05-10-first-blog-post.md
@@ -1,10 +1,17 @@
 ---
 layout: post
-title: My First Blog Post
+title: Starting Fresh
 date: 2018-05-10
+tagline: New website, new blog.
+background-image: tile45.jpg
 ---
-This is my first blog post. Just wanted to update everybody. You can visit my website by clicking [here](http://maxwirt.com). I hope its a nice day today. I've been really sad lately.
+<div class="blog-text">
+	<p>This is my first blog post. Just wanted to update everybody. You can visit my website by clicking [here](http://maxwirt.com). I hope its a nice day today. I've been really sad lately.
+	</p>
+</div>
 
 {% include blog/video.html ratio="16by9" url="https://www.youtube.com/embed/v1CiINFsBkY" %}
 
-I love watching this video because it makes me think of growing up as a kid.
+<div class="blog-text">
+	<p>I love watching this video because it makes me think of growing up as a kid.</p>
+</div>
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,36 @@
 WebFont.load({
     google: {
-        families: ['Gothic A1:400,700']
+        families: ['Asap:400,400i,600,600i,700,700i','Oxygen Mono']
     }
 });
 
+
 // Sidebar random background colors
 
-$(document).ready(function(){
-  var colors = ["#EF5350","#EC407A","#AB47BC","#7E57C2","#5C6BC0","#42A5F5","#29B6F6","#26C6DA","#26A69A","#66BB6A","#9CCC65","#D4E157","#FFEE58","#FFCA28","#FFA726","#FF7043"];  
-  $(".nav-item").each(function() {
-  	var rand = Math.floor(Math.random()*colors.length);
-  	$(this).css("background-color", colors[rand]);
+$(document).ready(function() {
+	  var colors = ["#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6", "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9", "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0", "#FBE9E7"];
+  $(".randombg").each(function() {
+    var rand = Math.floor(Math.random() * colors.length);
+    $(this).css("background-color", colors[rand]);
+  });
+});
+  
+/*
+  var colors = ["#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6", "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9", "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0", "#FBE9E7"];
+  $(".randombgcolor").each(function() {
+    var rand = Math.floor(Math.random() * colors.length);
+    $(this).css("background-color", colors[rand]);
 
   });
-  });             
+  $('.randombg').hover(
+    function() { $(this).addClass('randombgcolor') },
+    function() { $(this).removeClass('randombgcolor') }
+  );
+  
+});
 
 
-
-
-
+*/
 
 
 

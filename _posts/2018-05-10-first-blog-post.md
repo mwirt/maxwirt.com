b85a190a151bff68fd4f4045dd1214e44f6f3fcb
Max Wirt <max@sincerely.tv>
2018-05-18 08:26:21 -0400

    added responsive menu

--- _posts/2018-05-10-first-blog-post.md
+++ _posts/2018-05-10-first-blog-post.md
@@ -2,15 +2,16 @@
 layout: post
 title: Starting Fresh
 date: 2018-05-10
-tagline: New website, new blog.
+post-tagline: New website, new blog.
 background-image: tile45.jpg
+post-header-image:
 ---
 <div class="blog-text">
 	<p>This is my first blog post. Just wanted to update everybody. You can visit my website by clicking [here](http://maxwirt.com). I hope its a nice day today. I've been really sad lately.
 	</p>
 </div>
 
-{% include blog/video.html ratio="16by9" url="https://www.youtube.com/embed/v1CiINFsBkY" %}
+{% include video.html ratio="16by9" url="https://www.youtube.com/embed/v1CiINFsBkY" %}
 
 <div class="blog-text">
 	<p>I love watching this video because it makes me think of growing up as a kid.</p>
--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -4,8 +4,15 @@ WebFont.load({
     }
 });
 
+function openNav() {
+    document.getElementById("mySidenav").style.marginLeft = "0";
+}
 
-// Sidebar random background colors
+function closeNav() {
+    document.getElementById("mySidenav").style.marginLeft = "-260px";
+}
+
+// random background colors
 
 $(document).ready(function() {
 	  var colors = ["#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6", "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9", "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0", "#FBE9E7"];
@@ -15,25 +22,6 @@ $(document).ready(function() {
   });
 });
   
-/*
-  var colors = ["#FFEBEE", "#FCE4EC", "#F3E5F5", "#EDE7F6", "#E8EAF6", "#E3F2FD", "#E1F5FE", "#E0F7FA", "#E0F2F1", "#E8F5E9", "#F1F8E9", "#F9FBE7", "#FFFDE7", "#FFF8E1", "#FFF3E0", "#FBE9E7"];
-  $(".randombgcolor").each(function() {
-    var rand = Math.floor(Math.random() * colors.length);
-    $(this).css("background-color", colors[rand]);
-
-  });
-  $('.randombg').hover(
-    function() { $(this).addClass('randombgcolor') },
-    function() { $(this).removeClass('randombgcolor') }
-  );
-  
-});
-
-
-*/
-
-
-
 
 
 
